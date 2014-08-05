4531b2907a59f46fcc09f324f7072d81797b6885
Ralph Giles <giles@thaumas.net>
2014-08-04 17:03:35 -0700

    Pretty-print output.

--- web/list.js
+++ web/list.js
@@ -2,14 +2,23 @@ var net = require('net');
 
 var client = net.connect({port: 21012}, function() {
   // On connect.
-  console.log("connected");
+  console.log('client connected');
   client.write('{"action": "list", "seq": 0}\n');
 });
 client.on('data', function(data) {
-  console.log(data.toString());
+  result = JSON.parse(data);
+  if (result.action == 'list') {
+    console.log ('Available elements:');
+    elements = result.result[0];
+    for (id in elements) {
+      console.log('  ' + elements[id])
+    }
+  } else {
+    console.log('misunderstood response');
+    console.log(data.toString());
+  }
   client.end();
 });
 client.on('end', function() {
   console.log('client disconnected');
 });
-
