29305c77dea7de9fa59870ca9ce6f89c55aec676
Ralph Giles <giles@thaumas.net>
2014-08-20 02:59:49 -0700

    Quick parser for pulling out clock and timestamp.
    We parse telemtry blocks to get the feed aggregator's
    timestamp and station indication, then pull the
    corrsponding spacecraft clock (SCLK) value out of
    the frame when it's available.
    
    For continuous telemetry the clock seems to increment
    once per frame.

---
+++ cal/clock.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Quick parser for spacecraft clock calibration
+
+import sys
+
+for line in sys.stdin.readlines():
+  words = line.split()
+  if len(words) == 0:
+    continue
+  if words[0] == 'Frame':
+    timestamp = words[5][1:-1];
+    if (len(words) == 10):
+      station = words[9]
+    else:
+      station = 'unknown'
+    buffer = []
+  else:
+    for byte in words:
+      buffer.append(int(byte, 16))
+  if len(buffer) >= 128:
+    frame_number = buffer[60]
+    if (frame_number & 3 == 0):
+      sclk = buffer[61] << 16 | buffer[62] << 8 | buffer[63]
+    print('\t'.join(map(str, [timestamp, sclk, frame_number, station])))
